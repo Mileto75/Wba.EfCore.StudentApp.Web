0d933d6e67a510ceddc524674db13e8a512edb0a
Mileto Di Marco <mileto.di.marco@howest.be>
2021-03-29 06:23:34 +0200

    Add project files.

---
+++ Wba.EfCore.StudentApp.Domain/Entities/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wba.EfCore.StudentApp.Domain.Entities
+{
+    public class Course
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        //navigation property
+        public Teacher Teacher { get; set; }
+        public long? TeacherId { get; set; }
+    }
+}
---
+++ Wba.EfCore.StudentApp.Web/Data/SchoolDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wba.EfCore.StudentApp.Domain.Entities;
+
+namespace Wba.EfCore.StudentApp.Web.Data
+{
+    public class SchoolDbContext : DbContext
+    {
+        //dbsets stellen onze tabellen voor
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public SchoolDbContext
+            (DbContextOptions<SchoolDbContext> options)
+            :base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
